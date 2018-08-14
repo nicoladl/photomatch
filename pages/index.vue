68572787b98e7ea82b8c98cc3cde143e519d2571
Nicola <nicola.delazzari@octoplus.ch>
2018-08-14 17:52:09 +0200

    added static image detect and add points

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,111 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        photomatch
-      </h1>
-      <h2 class="subtitle">
-        match your body with great photo
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <section>
+    <h1>hp</h1>
+    <!-- <div class="image"></div> -->
+    <div class="image">
+      <img class="image" src="image.jpg" alt="test">
     </div>
   </section>
 </template>
 
-<script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
-  }
-}
-</script>
-
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.image {
+  position: relative;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.part {
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: red;
 }
 </style>
 
+
+<script>
+import * as posenet from '@tensorflow-models/posenet'
+
+export default {
+  mounted () {
+    var imageScaleFactor = 0.5;
+    var flipHorizontal = false;
+    var outputStride = 16;
+    var maxPoseDetections = 2;
+
+    // function loadImage(url) {
+    //   return new Promise((resolve, reject) => {
+    //     let img = new Image();
+    //     img.addEventListener('load', e => resolve(img));
+    //     img.addEventListener('error', () => {
+    //       reject(new Error(`Failed to load image's URL: ${url}`));
+    //     });
+    //     img.src = url;
+    //   });
+    // }
+
+    // // load the image, and append it to the element id="image-holder"
+    // loadImage('https://loremflickr.com/1200/640/federer')
+    //   .then(function (img) {
+    //     document.querySelector('.image').appendChild(img)
+
+    //     console.log(img)
+
+    //     posenet.load().then(function(net) {
+    //       return net.estimateMultiplePoses(img, 0.5, flipHorizontal, outputStride, maxPoseDetections)
+    //     }).then(function(poses){
+    //       console.log(poses);
+
+    //       for (let i = 0; i < poses.length; i++) {
+    //         const el = poses[i];
+    //         console.log(el.keypoints)
+
+    //         for (let k = 0; k < el.keypoints.length; k++) {
+    //           const childEl = el.keypoints[k];
+    //           console.log(childEl)
+
+    //           let part = document.createElement('div')
+    //           part.className = 'part '
+    //           part.className += childEl.part
+
+    //           part.style.top = parseInt(childEl.position.y) + 'px'
+    //           part.style.left = parseInt(childEl.position.x) + 'px'
+
+    //           console.log(part)
+
+    //           document.querySelector('section').appendChild(part)
+    //         }
+    //       }
+    //     })
+    //   })
+    //   .catch(error => console.error(error));
+
+    posenet.load().then(function(net) {
+        return net.estimateMultiplePoses(document.querySelector('.image img'), 0.5, flipHorizontal, outputStride, maxPoseDetections)
+      }).then(function(poses){
+        console.log(poses);
+
+        for (let i = 0; i < poses.length; i++) {
+          const el = poses[i];
+          console.log(el.keypoints)
+
+          for (let k = 0; k < el.keypoints.length; k++) {
+            const childEl = el.keypoints[k];
+            console.log(childEl)
+
+            let part = document.createElement('div')
+            part.className = 'part '
+            part.className += childEl.part
+
+            part.style.top = parseInt(childEl.position.y) + 'px'
+            part.style.left = parseInt(childEl.position.x) + 'px'
+
+            console.log(part)
+
+            document.querySelector('section').appendChild(part)
+          }
+        }
+      })
+  }
+}
+</script>
